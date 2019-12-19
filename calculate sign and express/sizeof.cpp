efe0daa504b0ceb6e14708c8702c0d2b4ac65ef5
Monicaofak <56330821+Monicaofak@users.noreply.github.com>
2019-12-19 22:03:33 +0800

    Update sizeof.cpp

--- calculate sign and express/sizeof.cpp
+++ calculate sign and express/sizeof.cpp
@@ -11,3 +11,15 @@ int main()
   cout<< sizeof('\n') <<endl; //char
   cout<< sizeof("abc")<<endl; //string 后面默认\0 所以加一
  }
+
+
+#include <iostream>
+using namespace std;
+int main()
+ {
+ int num = 5;
+ cout << sizeof(num++)<<endl;  //sizeof 是一个运算符 ++运算不执行 只用来量里面的东西其他不管
+ cout<< num<<endl;
+ }
+
+

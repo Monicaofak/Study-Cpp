802becab9866c691211884591938ef8a5913daaf
Monicaofak <56330821+Monicaofak@users.noreply.github.com>
2019-12-19 14:39:09 +0800

    Create true1&false0.cpp

---
+++ calculate sign and express/true1&false0.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   //IN language C,0 means false,no 0 means true
+    //IN C++,bool type,bool just has true and false
+    cout << boolalpha;  //no this display 0;1
+    cout << "15 > 88 ?"<< (15 > 88)<<endl;
+    cout << "16 < 99 ?"<< (16 < 99)<<endl;
+}

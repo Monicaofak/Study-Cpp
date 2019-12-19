2580ab4374e09302663e9e21a8c4ef856575f140
Monicaofak <56330821+Monicaofak@users.noreply.github.com>
2019-12-19 16:45:44 +0800

    Create if else and multiple it.cpp

---
+++ calculate sign and express/if else and multiple it.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+ {
+  //Write a program to judge whether the word user inter is true disk sign
+  char pan ='\0'; //默认空字符设置为空 空字符：ASCCL码为0的字符
+  cout<<"please inter a sign"<<endl;
+  cin >> pan;
+  // 'A'~'Z'
+  if ( 'A' <= pan && pan <= 'Z')  //65 <=pan && pan <= 65+26
+
+  {
+      cout<<"legal disk sign!"<<endl;
+  }
+    else
+    {
+    cout<<"illegal disk sign!"<<endl;
+    }
+  }
+  
+****
+
+#include <iostream>
+using namespace std;
+int main()
+ {     //shopping chart
+ double price1 = 35000.0;
+ double hemes = 11044.5;
+ double chanel = 1535.0;
+ double total = 0;
+ total = price1 + hemes + chanel;
+     //if add up to 5w,30% off,else 10% off
+ if (total > 50000)
+ {
+ cout<<total * 0.7<<endl;
+ }
+  else
+  {
+  cout<<total*0.1<<endl;
+  }
+ }
+ 
+ ****
+ 
+ #include <iostream>
+using namespace std;
+int main()
+ {
+   //
+   double flowerPrice;
+   //print story
+   cout <<"the darkness before dawn fades gradually,a lightness between sky and sea is wonderful"<<endl;
+   cout <<"boy sent flowers to girl ,girl asked the price"<<endl;
+   cin>> flowerPrice;
+   if(flowerPrice >= 5000 &&  flowerPrice <10000)
+   {
+   cout<<"I like you!"<<endl;
+   }
+   else if(flowerPrice >= 10000 && flowerPrice <= 50000)
+   {
+   cout<<"I love you!"<<endl;
+   }
+   else
+   {
+   cout<<"marry me"<<endl;
+   }
+ }
+
+
+
+ 

a2ccfedce9c45ff79a0dabeaa7f6b06fd588938c
Monicaofak <56330821+Monicaofak@users.noreply.github.com>
2020-01-09 12:42:34 +0800

    Update data type.cpp

--- varible quantity & data type & calculate sign/data type.cpp
+++ varible quantity & data type & calculate sign/data type.cpp
@@ -23,3 +23,73 @@ int main()
 //还有各种数据类型的无符号版
 //其他：size_t,枚举类型，自定义类型，指针类型，空类型
 //关键字全为大写时为常量
+
+//float 6-7为有效数字  小数点前后加起来6=7位
+# include <iostream>
+using namespace std;
+int main()
+ {
+ float num =12345.12345678f;  
+ cout<<num<<endl;
+ return 0;
+ }
+ 
+
+//已知圆柱的半径和高，求圆柱的体积
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+ {
+ const double PI = 3.14;
+ float radius = 4.5;
+ float height = 90.0;
+ double volume = PI*pow(radius,2)*height;
+ cout<<volume;
+ return 0;
+ }
+//定义float可以在结尾加一个f
+//cmath函数库定义了数学函数
+
+#include <iostream>
+#include <iomanip>  //控制符的头文件
+using namespace std;
+int main()
+ {
+ cout<<fixed;  //控制显示的精度 1.以小数显示2.控制位数
+ cout<<setprecision(2);
+ double doubleNum =10.0/3.0;
+ cout<<doubleNum<<endl;
+ cout<<doubleNum*1000000<<endl;  //数字太大，显示了科学技术法
+ cout<<setw(8)<<"|"<<3.14<<"|"<<endl; //setw 设定宽度，只管他后面的符号
+ return 0;
+ }
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+ cout<<sizeof(double)<<endl;
+ cout<<sizeof(long double)<<endl;   //占的字节越长，精度越高
+ cout<<sizeof(3.14)<<endl;  //C++中，小数默认是double类型
+ cout<<sizeof(3.14f)<<endl;
+ return 0;
+}
+
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+ //演示单精度浮点型和双精度浮点型精度问题
+ float numFloat =10/3.0;
+ double numDouble =10/3.0;
+ cout<<fixed; //让浮点型以固定的方式显示
+ cout<<setprecision(2);//控制显示小数位数
+ cout<<numFloat * 10000<<endl;   //float显示错误（精度不够）
+ cout<<numDouble * 10000<<endl;
+ return 0;
+}
+
